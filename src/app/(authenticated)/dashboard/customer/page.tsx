bf5b15e447d808295ba448d60f04321a4bfff273
Oscar Kemuel <oscar.kemuel5@gmail.com>
2023-05-10 08:36:41 -0300

    feat(customer-add): add new row in the customer page

--- src/app/(authenticated)/dashboard/customer/page.tsx
+++ src/app/(authenticated)/dashboard/customer/page.tsx
@@ -154,11 +154,11 @@ function CustomerPage() {
                     <Card.Body>
                       <Card.Title>{place.name}</Card.Title>
                       <Card.Text className="m-0">
-                          {address.street}, {address.neighborhood} -{" "}
-                          {address.number}
+                        {address.street}, {address.neighborhood} -{" "}
+                        {address.number}
                       </Card.Text>
                       <Card.Text>
-                          {address.city} - {address.state}
+                        {address.city} - {address.state}
                       </Card.Text>
 
                       <div className="d-flex align-items-center gap-2">
@@ -188,6 +188,67 @@ function CustomerPage() {
               {myPlacesIsFetchedAfterMount && places.length === 0 && (
                 <Alert variant="warning" className="m-auto">
                   You don't have any place yet!
+                </Alert>
+              )}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <div className="d-flex align-items-center justify-content-between mb-2">
+            <h3 className="m-0">My Requests</h3>
+            <Button
+              disabled
+              variant="success"
+              onClick={() => navigateTo("/dashboard/customer/place/add")}
+            >
+              New Request
+            </Button>
+          </div>
+          <Card>
+            <Card.Body className="d-flex gap-3">
+              {[].map((place) => {
+                return (
+                  <Card key={"place.id"} style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>{"place.name"}</Card.Title>
+                      <Card.Text className="m-0">
+                        {"address.street"}, {"address.neighborhood"} -{" "}
+                        {"address.number"}
+                      </Card.Text>
+                      <Card.Text>
+                        {"address.city"} - {"address.state"}
+                      </Card.Text>
+
+                      <div className="d-flex align-items-center gap-2">
+                        <Button
+                          variant="primary"
+                          onClick={() =>
+                            navigateTo(
+                              `/dashboard/customer/place/${"place.id"}/edit`
+                            )
+                          }
+                        >
+                          <MdEdit />
+                        </Button>
+                        <Button
+                          variant="danger"
+                          onClick={() => deletePlaceMutation.mutate("place.id")}
+                          disabled={deletePlaceMutation.isLoading}
+                        >
+                          <MdDelete />
+                        </Button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+
+              {true && (
+                <Alert variant="warning" className="m-auto">
+                  You don't have any request yet!
                 </Alert>
               )}
             </Card.Body>
